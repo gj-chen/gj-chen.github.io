53a3a4ec6b4d5c70c2e4e2ee9fa5a72cc9ad6f73
Gloria <gjchen@ucdavis.edu>
2015-09-05 18:31:41 -0700

    redid website again

--- js/main.js
+++ js/main.js
@@ -1,49 +1,70 @@
-jQuery(document).ready(function($){
-	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
 
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
+function main() {
+
+(function () {
+   'use strict';
+
+   /*====================================
+    Page a Link Smooth Scrolling 
+    ======================================*/
+    $('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Menu Active Calling Scroll Spy
+    ======================================*/
+    $('body').scrollspy({ 
+      target: '.navmenu',
+      offset: 80,
+    });
+
+
+    /* ==============================================
+	Testimonial Slider
+	=============================================== */ 
+
+	$(document).ready(function() {
+	 
+	  $("#testimonial").owlCarousel({
+	 
+	      navigation : false, // Show next and prev buttons
+	      slideSpeed : 300,
+	      paginationSpeed : 400,
+	      singleItem:true,
+	      autoHeight : true
+	 
+	      // "singleItem:true" is a shortcut for:
+	      // items : 1, 
+	      // itemsDesktop : false,
+	      // itemsDesktopSmall : false,
+	      // itemsTablet: false,
+	      // itemsMobile : false
+	 
+	  });
+	 
 	});
-
-	//smooth scroll to the section
-	navigationItems.on('click', function(event){
  
-        smoothScroll($(this.hash));
-    });
-    //smooth scroll to second section
-    $('.cd-scroll-down').on('click', function(event){
-  
-        smoothScroll($(this.hash));
-    });
 
-    //open-close navigation on touch devices
-    $('.touch .cd-nav-trigger').on('click', function(){
-    	$('.touch #cd-vertical-nav').toggleClass('open');
-  
-    });
-    //close navigation on touch devices when selectin an elemnt from the list
-    $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
-    });
+ 
 
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
-		});
-	}
 
-	function smoothScroll(target) {
-        $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
-        );
-	}
-});
+}());
+
+
+}
+main();
